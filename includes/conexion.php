4e9dfcfab7dfee9c3583a7ec806a9d369568dd95
Yhavet Alaniz <104223938+yhavet@users.noreply.github.com>
2024-09-15 22:11:56 -0300

    Update conexion.php

--- includes/conexion.php
+++ includes/conexion.php
@@ -1,3 +1,15 @@
 <?php
 
 $host = 'localhost'; 
+$user = 'root'; 
+$db = 'sistema-escolar'; 
+$pass = ''; 
+
+
+try {
+
+    $pdo = new PDO('mysql:host='.$host.';dbname=' .$db. ';charset=utf8', $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(Exception $e) {
+    echo 'ERROR: ' . $e->getMessage();
+}
